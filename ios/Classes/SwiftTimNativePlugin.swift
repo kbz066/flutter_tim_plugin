c348d501c5b0d38ed9be40389dbde11a37fec8ac
kbz_home <18211571509@163.com>
2019-12-23 07:18:38 -0800

    支持调用oc

--- ios/Classes/SwiftTimNativePlugin.swift
+++ ios/Classes/SwiftTimNativePlugin.swift
@@ -1,15 +1,21 @@
 import Flutter
 import UIKit
 
+
 public class SwiftTimNativePlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_tim_plugin", binaryMessenger: registrar.messenger())
+        
+
+    let channel = FlutterMethodChannel(name: "tim_plugin", binaryMessenger: registrar.messenger())
     let instance = SwiftTimNativePlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     
-    result("iOS " + UIDevice.current.systemVersion)
+    
+    print("tim_plugin--------_>")
+
+    //result("iOS " + UIDevice.current.systemVersion)
   }
 }
